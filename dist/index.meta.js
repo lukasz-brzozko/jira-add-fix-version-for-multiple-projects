327d8592e93bfa96ee4e7513e2a24432894ad670
Łukasz Brzózko <lukasz9099@gmail.com>
2024-05-04 15:52:09 +0200

    feat: add dist files

--- dist/index.meta.js
+++ dist/index.meta.js
@@ -1,12 +1,14 @@
 // ==UserScript==
-// @name         Userscript starter
-// @namespace    https://github.com/lukasz-brzozko/userscript-starter
-// @version      2024-02-21
-// @description  Creates a new userscript
+// @name         Jira Add Fix Version For Multiple Projects
+// @namespace    https://github.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects
+// @version      2024-05-04
+// @description  Allows to add one fix version for different projects simultaneously
 // @author       Łukasz Brzózko
-// @match        https://github.com/
-// @icon         https://github.githubassets.com/favicons/favicon.png
-// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.meta.js
-// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.user.js
-// @grant        none
+// @match        https://jira.nd0.pl/*
+// @exclude      https://jira.nd0.pl/plugins/servlet/*
+// @icon         https://jira.nd0.pl/s/a3v501/940003/1dlckms/_/images/fav-jsw.png
+// @resource styles    https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/styles.css
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/index.user.js
+// @grant        GM_getResourceText
 // ==/UserScript==
--- src/index.meta.js
+++ src/index.meta.js
@@ -4,10 +4,11 @@
 // @version      2024-05-04
 // @description  Allows to add one fix version for different projects simultaneously
 // @author       Łukasz Brzózko
-// @match        https://jira.nd0.pl/projects*
+// @match        https://jira.nd0.pl/*
 // @exclude      https://jira.nd0.pl/plugins/servlet/*
 // @icon         https://jira.nd0.pl/s/a3v501/940003/1dlckms/_/images/fav-jsw.png
+// @resource styles    https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/styles.css
 // @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/index.meta.js
 // @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/index.user.js
-// @grant        none
+// @grant        GM_getResourceText
 // ==/UserScript==
