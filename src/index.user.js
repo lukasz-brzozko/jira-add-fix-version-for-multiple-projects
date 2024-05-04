0d5edd6730745dab440433ab9debe54e98d10faf
Łukasz Brzózko <lukasz9099@gmail.com>
2024-05-04 09:24:50 +0200

    feat: handle navigation without page refreshing

--- src/index.user.js
+++ src/index.user.js
@@ -4,7 +4,7 @@
 // @version      2024-05-03
 // @description  Allows to add one fix version for different projects simultaneously
 // @author       Łukasz Brzózko
-// @match        https://jira.nd0.pl/projects*
+// @match        https://jira.nd0.pl/*
 // @exclude      https://jira.nd0.pl/plugins/servlet/*
 // @icon         https://jira.nd0.pl/s/a3v501/940003/1dlckms/_/images/fav-jsw.png
 // @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/index.meta.js
@@ -93,6 +93,10 @@
 
   const getCurrentProjectName = () => {
     return window.location.pathname.split("/").at(-1);
+  };
+
+  const getIsProjectsPage = () => {
+    return window.location.pathname.split("/").at(1) === "projects";
   };
 
   const displayMessage = ({ type = "info", title, content }) => {
@@ -261,6 +265,10 @@
     );
   };
 
+  const handleNavigate = () => {
+    if (getIsProjectsPage()) return init();
+  };
+
   const init = async () => {
     try {
       await lookForAppContainer();
@@ -272,5 +280,7 @@
     renderUiElements();
   };
 
-  init();
+  if (getIsProjectsPage()) return init();
+
+  window.navigation.addEventListener("navigate", handleNavigate);
 })();
