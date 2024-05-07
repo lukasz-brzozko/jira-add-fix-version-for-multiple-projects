350a1625658843a85341dca09297c6ae52869d4c
Łukasz Brzózko <lukasz9099@gmail.com>
2024-05-07 18:06:15 +0200

    feat: update build script

--- src/index.user.js
+++ src/index.user.js
@@ -1,3 +1,18 @@
+// ==UserScript==
+// @name         Jira Add Fix Version For Multiple Projects
+// @namespace    https://github.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects
+// @version      2024-05-07
+// @description  Allows to add one fix version for different projects simultaneously
+// @author       Łukasz Brzózko
+// @match        https://jira.nd0.pl/*
+// @exclude      https://jira.nd0.pl/plugins/servlet/*
+// @icon         https://jira.nd0.pl/s/a3v501/940003/1dlckms/_/images/fav-jsw.png
+// @resource styles    https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/styles.css
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/jira-add-fix-version-for-multiple-projects/main/dist/index.user.js
+// @grant        GM_getResourceText
+// ==/UserScript==
+
 (function () {
   "use strict";
 
